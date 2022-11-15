55fd3e40f14d7d591859f9df8b11d78a48409bbd
Juan David Castro <me@juandc.co>
2022-11-15 09:31:04 -0500

    useState

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,15 +1,18 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');
+
+  console.log('Los usuarios buscan todos de ' + searchValue);
+
   return (
     <input
       placeholder="Cortar cebolla"
       className="TodoSearch"
+      value={searchValue}
       onChange={(event) => {
-        console.log('Escribiste en el TodoSearch');
-        console.log(event);
-        console.log(event.target);
-        console.log(event.target.value);
+        setSearchValue(event.target.value);
       }}
     />
   );
