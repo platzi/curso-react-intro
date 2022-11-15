40b0ed90fe37b3d3971010bcc96290d6feaf4b59
Juan David Castro <me@juandc.co>
2022-11-15 10:03:59 -0500

    contando todos

--- src/App.js
+++ src/App.js
@@ -10,13 +10,30 @@ const defaultTodos = [
   { text: 'Tomar el Curso de Intro a React.js', completed: false },
   { text: 'Llorar con la Llorona', completed: false },
   { text: 'LALALALALA', completed: false },
+  { text: 'Usar estados derivados', completed: true },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(
+    todo => !!todo.completed
+  ).length;
+  const totalTodos = todos.length;
+
+  console.log('Los usuarios buscan todos de ' + searchValue);
+  
   return (
     <>
-      <TodoCounter completed={16} total={25} />
-      <TodoSearch />
+      <TodoCounter
+        completed={completedTodos}
+        total={totalTodos} 
+      />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
         {defaultTodos.map(todo => (
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = React.useState('');
-
-  console.log('Los usuarios buscan todos de ' + searchValue);
-
+function TodoSearch({
+  searchValue,
+  setSearchValue,
+}) {
   return (
     <input
       placeholder="Cortar cebolla"
