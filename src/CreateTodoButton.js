6be16ef432d4ddbcf23fcb4dd0b938fcf945ab41
Juan David Castro <me@juandc.co>
2022-11-15 09:15:37 -0500

    Eventos

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,16 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton() {
   return (
-    <button className="CreateTodoButton">+</button>
+    <button
+      className="CreateTodoButton"
+      onClick={
+        (event) => {
+          console.log('le diste click')
+          console.log(event)
+          console.log(event.target)
+        }
+      }
+    >+</button>
   );
 }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -5,6 +5,12 @@ function TodoSearch() {
     <input
       placeholder="Cortar cebolla"
       className="TodoSearch"
+      onChange={(event) => {
+        console.log('Escribiste en el TodoSearch');
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+      }}
     />
   );
 }
