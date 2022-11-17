29c391c6dbfa96e9ebe5adc75f663f504a777718
Juan David Castro <me@juandc.co>
2022-11-17 10:48:42 -0500

    Deploy

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -25,7 +25,7 @@ function TodoForm() {
 
   return (
     <form onSubmit={onSubmit}>
-      <label>Esribe tu nuevo TODO</label>
+      <label>Escribe tu nuevo TODO</label>
       <textarea
         placeholder="Cortar cebolla para el almuerzo"
         value={newTodoValue}
