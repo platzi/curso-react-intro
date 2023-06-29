9d8afc4c6b0cbf02a1c3f51bd8793f4d9b514d99
Santiago Millan <104397283+SantiagoNivelics@users.noreply.github.com>
2023-06-29 13:24:10 -0500

    Clase 6

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -5,6 +5,12 @@ function TodoSearch() {
     <input
       placeholder="Cortar cebolla"
       className="TodoSearch"
+      onChange={(event) =>{
+        console.log("escribiuste ")
+        console.log(event)
+        console.log(event.target)
+        console.log(event.target.value)
+      }}
     />
   );
 }
