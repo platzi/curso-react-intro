bf03dfb2c36dbf1c50a18d659cc6192104eb3eb8
fspind71 <fspind@gmail.com>
2023-06-29 19:39:33 -0300

    Clase 3: Creamos la estructura basica del ToDo

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
-import logo from './platzi.webp';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoItem } from './TodoItem';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Tareas Pendientes</h1>
+      <TodoCounter/>
+      <TodoSearch/>
+      <TodoList>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+      </TodoList>
 
-      <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tengo que aprender React
-        </a>
-      </header>
+      <CreateTodoButton/>
+      
+      
     </div>
   );
 }
 
-function TodoItem(){
-  return(
-    <li>
-      <span>V</span>
-      <p>Llorar con la llorona</p>
-      <span>X</span>
-    </li>
-  );
-}
+
 
 export default App;
---
+++ src/TodoCounter.js
@@ -0,0 +1,9 @@
+function TodoCounter(){
+    return(
+        <h1>
+            Has completado 3 de 5 tareas
+        </h1>
+    );
+}
+
+  export { TodoCounter };
