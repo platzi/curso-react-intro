934ca49a4109b0365e6d2bfa7319e5c6b6bee53c
Santiago Millan <104397283+SantiagoNivelics@users.noreply.github.com>
2023-07-03 00:06:59 -0500

    Clase 10 se puede hacer la actividad recomendad

--- src/App.js
+++ src/App.js
@@ -23,6 +23,29 @@ function App() {
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText =todo.text.toLowerCase();
+      const searchText =searchValue.toLowerCase();
+      return todoText.includes(searchText)
+    }
+  )
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos]
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text)
+    newTodos[todoIndex].completed = true
+    setTodos(newTodos)
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos]
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text)
+    newTodos.splice(todoIndex, 1)
+    setTodos(newTodos)
+  }
+
+
   console.log("searchValue",searchValue)
 
   return (
@@ -35,8 +58,8 @@ function App() {
       />
 
       <TodoList>
-        {defaultTodos.map(todo => (
-          <TodoItem key={todo.key} text={todo.text} completed={todo.completed}/>
+        {searchedTodos.map(todo => (
+          <TodoItem key={todo.key} text={todo.text} completed={todo.completed} onComplete={()=>completeTodo(todo.text)} onDelete={()=> deleteTodo(todo.text)}/>
         ))}
         
       </TodoList>
