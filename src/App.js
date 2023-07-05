040b4b5ba176ab57d9cd41ac0745390a8c8bf164
fspind71 <fspind@gmail.com>
2023-07-05 08:49:37 -0300

    Se agrega funcionalidad buscar Todo, Completar y eliminar Todos

--- src/App.js
+++ src/App.js
@@ -21,7 +21,33 @@ function App() {
   const completedTodos = todos.filter(todo=>!!todo.completed).length;
   const totalTodos = todos.length;
 
-  console.log ('Los usuarios buscan ToDos de ' + searchValue);
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLocaleLowerCase();
+      const searchText = searchValue.toLocaleLowerCase();
+
+      return todoText.includes(searchText)
+      
+    }
+  );
+
+  const completeTodo = (text) =>{
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) =>{
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
 
   return (
    <>
@@ -36,11 +62,13 @@ function App() {
       />
 
       <TodoList >
-        {defaultTodos.map(todo =>(
+        {searchedTodos.map(todo =>(
           <TodoItem 
           key={todo.text} 
           text={todo.text}
           completed={todo.completed}
+          onComplete = {()=>completeTodo(todo.text)}
+          onDelete = {()=>deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
