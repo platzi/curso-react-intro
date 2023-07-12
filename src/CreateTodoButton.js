f1edb112808dfadf9adbb59c9aa80545d6df9e64
JuanF3r <juanfercdc@gmail.com>
2023-07-12 17:29:33 -0500

    feat: update pages(button and search)

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,9 +2,15 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton() {
     return (
-      <button className='CreateTodoButton'>
-        +
-      </button>
+      <button className='CreateTodoButton'
+      onClick={
+        (event) => {
+          console.log('le diste click')
+          console.log(event)
+          console.log(event.target)
+          }
+        }
+      >+</button>
     )
   }
   
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,14 @@ import './TodoSearch.css';
 
 function TodoSearch() {
     return (
-      <input className='TodoSearch ' placeholder="Cortar cebolla" />
+      <input 
+        className='TodoSearch'
+        placeholder="Cortar cebolla"
+        onChange={(event) => {
+          console.log('Escribiste en el todo search');
+          console.log(event.target.value);
+        }}
+        />
     )
   }
   
