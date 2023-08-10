a17a66df2cf2efc06024326551897152b51bf8f4
leo tatico <leoandrest40@gmail.com>
2023-08-09 22:43:14 -0500

    deployy

--- src/TodoContext/useLocalStore.js
+++ src/TodoContext/useLocalStore.js
@@ -3,36 +3,30 @@ import React from 'react';
 function useLocalStorage(itemName, initialValue) {
   const [item, setItem] = React.useState(initialValue);
   const [loading, setLoading] = React.useState(true);
-  const [error, setErorr] = React.useState(false);
-
-
+  const [error, setError] = React.useState(false);
   
-
   React.useEffect(() => {
     setTimeout(() => {
-      try{
+      try {
         const localStorageItem = localStorage.getItem(itemName);
-  
-        let parsedItem
+    
+        let parsedItem;
   
         if (!localStorageItem) {
-        localStorage.setItem(itemName, JSON.stringify(initialValue));
-        parsedItem = initialValue;
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
         } else {
           parsedItem = JSON.parse(localStorageItem);
           setItem(parsedItem);
         }
-         
+  
         setLoading(false);
-      } catch(error){
-        setLoading(false)
-        setError(true)
+      } catch(error) {
+        setLoading(false);
+        setError(true);
       }
     }, 2000);
-  }, []);
-
-  
-  
+  });
 
   const saveItem = (newItem) => {
     localStorage.setItem(itemName, JSON.stringify(newItem));
@@ -44,12 +38,13 @@ function useLocalStorage(itemName, initialValue) {
     saveItem,
     loading,
     error,
-  }; // se retorna un objeto porque en los hooks los arrays funcionan distinto,
-     // para cuando se llame el custom hooks desde cualquier componente ya sea mas facil saber cual es cual.
+  };
 }
 
 export { useLocalStorage };
 
+
+// localStorage.removeItem('TODOS_V1');
 
 // const defaultTodos = [
 //   { text: 'Cortar cebolla', completed: true },
@@ -60,4 +55,3 @@ export { useLocalStorage };
 // ];
 
 // localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
-// localStorage.removeItem('TODOS_V1');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM  from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.render(<App />, document.getElementById('root'));
 root.render(<App />);
