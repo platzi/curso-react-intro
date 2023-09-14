095a330af5bdf5ec648f73ffbbeb9ca2fc64387e
Misael-GC <misael.gomez1623@gmail.com>
2023-09-14 10:36:33 -0600

    preparando manipulación del DOM

--- src/TodoCounter.jsx
+++ src/TodoCounter.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./TodoCounter.css"
 
 function TodoCounter({ total, completed }){
     return(
-        <h1>
-            Has completado {completed} de {total} TODOs
+        <h1 className="TodoCounter">
+            Has completado <span>{completed}</span> de <span>{total}</span> TODOs
         </h1>
     );
 }
