c0c408098edb47a6b5065cfbfa5876036e81458e
Misael-GC <misael.gomez1623@gmail.com>
2023-09-29 01:40:58 -0600

    add graph circle2

--- src/Graficos.jsx
+++ src/Graficos.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import 'chart.js/auto';
+import './Graficos'
 
 function Graficos({ total, completed, }) {
 
   const res = (completed / total)*100;
 
+  //grafico
+  const data = {
+    labels: ['Completado', 'Restante'],
+    datasets: [
+      {
+        data: [completed, total - completed],
+        backgroundColor: ['#61dafa', '#212529'],
+        hoverBackgroundColor: ['#61dafa', '#212529'],
+      },
+    ],
+  };
+
   return (
-    <section className="container mb-5">
-      <div className="grafico1">Soy un círculo O</div>
+    <section className="container mb-5" >
+      <div className='Circle'>
+        <Doughnut data={data} />
+      </div>
 
       <div className="progress" role="progressbar" aria-label="Default striped example" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
         <div className="progress-bar progress-bar-striped bg-info" style={{ width: `${res.toFixed(2)}%` }}>{ res }%</div>
       </div>
-      <div className="progress" role="progressbar" aria-label="Success striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-        <div className="progress-bar progress-bar-striped bg-black" style={{ width: '25%' }}>25%</div>
+      <div>
+        <p>Textos</p>
       </div>
-      <div className="progress" role="progressbar" aria-label="Info striped example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-        <div className="progress-bar progress-bar-striped bg-black" style={{ width: '50%' }}>50%</div>
-      </div>
-      <div className="progress" role="progressbar" aria-label="Warning striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-        <div className="progress-bar progress-bar-striped bg-info" style={{ width: '75%' }}>75%</div>
-      </div>
-      <div className="progress" role="progressbar" aria-label="Danger striped example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-        <div className="progress-bar progress-bar-striped bg-info" style={{ width: '100%' }}>100%</div>
+      <div>
+        <p>Textos</p>
       </div>
     </section>
   );
