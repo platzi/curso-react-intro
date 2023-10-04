cc6566b4ac0120fe82d0be35971d6bbfb648e38e
Misael-GC <misael.gomez1623@gmail.com>
2023-10-04 14:33:52 -0600

    react icons p1

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,14 +1,15 @@
 import './TodoItem.css'
+import { BsFillDashCircleFill, BsCheckSquareFill } from "react-icons/bs";
 
 function TodoItem(props) {
     return (
       <li className="list-group-item list-group-item-action list-group-item-dark TodoItem">
 
-        <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>[/]</span> 
+        <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}><BsCheckSquareFill/></span> 
 
         <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
 
-        <span className="Icon Icon-delete">x</span>
+        <span className="Icon Icon-delete"><BsFillDashCircleFill/></span>
       </li>
     );
   }
