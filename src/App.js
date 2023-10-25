78bba07b1dbea8ac60eb1ba2915efbd1fe6f9e69
Steven Calderon <101149701+JosCalCor22@users.noreply.github.com>
2023-10-25 22:02:20 +0000

    Adding my first commits in this project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './platzi.webp';
-import './App.css';
+import logo from "./platzi.webp";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
+        <p>Mi primera vez en este curso</p>
         <a
           className="App-link"
           href="https://platzi.com/reactjs"
@@ -18,7 +16,29 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <CountTodo />
+        <TodoItem />
+      </main>
     </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <li style={{ listStyle: "none" }}>
+      <span>V</span>
+      <p>Hacer el curso</p>
+      <span>X</span>
+    </li>
+  );
+}
+
+function CountTodo(){
+  return(
+    <h1>
+      Completaste 3 de 5 ToDo
+    </h1>
   );
 }
 
