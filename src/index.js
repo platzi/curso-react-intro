d1ef825245b29e73dcba688756f7f18753556086
Misael-GC <misael.gomez1623@gmail.com>
2023-11-12 09:33:42 -0600

    cambio a la version 17.1.0 de React y haciendo un solo ajuste para copilar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+//React 18
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+//React 17.0.2
+const root = document.getElementById('root');
+render(<App />, root);
