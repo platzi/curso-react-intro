e0aa83920d23d5f301bbf859255d595dd78194e9
Jairiana de Jesus <jairianadejesus2005@gmail.com>
2023-12-13 18:29:34 -0300

    Componentes y Elementos

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import logo from './platzi.webp';
 import './App.css';
 
 function App() {
-  return (
+  return ( // a partir de aqui no estamos incorporando html, sino JSX
     <div className="App">
+
+  {/* ¿ Cómo llamo a un componente? Escribiendo su nombre con la siguiente sintáxis < Componente1 />  */}
+    <TodoCount
+      completed={3}
+      total={5}
+    />
+    
+    <TodoItem/>
+    <TodoItem/>
+    <TodoItem/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,10 +26,26 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendamos React
         </a>
       </header>
     </div>
+  );
+}
+
+function TodoCount(props){
+  return(
+    <h1>Ya completaste {props.completed} de {props.total} TODO's</h1>
+  )
+}
+
+function TodoItem(){ //componente
+  return( //elemento
+    <li>
+      <span>V</span>
+      <p>Llorar con la llorona</p>
+      <span>X</span>
+    </li>
   );
 }
 
